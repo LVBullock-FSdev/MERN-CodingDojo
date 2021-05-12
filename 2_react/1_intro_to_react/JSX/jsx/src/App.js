9f5ce913369d6bc4e85ee524c8f3279dc698c2a9
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-05-11 23:47:43 -0400

    2-React --> 1-Intro to React --> First React With JSX

---
+++ 2_react/1_intro_to_react/JSX/jsx/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+        <ul>
+          <li>&#9672; Feed Smokie</li>
+          <li>&#9672; Take the boys to work</li>
+          <li>&#9672; Sew a quilt block</li>
+          <li>&#9672; Submit Discussion</li>
+          <li>&#9672; Watch Lectures</li>
+          <li>&#9672; Complete & Submit Assignment</li>
+          <li>&#9672; Watch YouTube - Craft Marathon</li>
+          <li>&#9672; Wake up from this coding nightmare!</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
