09ccb3aef3860bdfec5b47b78c2717e7b148383d
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-05-16 22:21:37 -0400

    2-React --> 2-Functional Components --> Hook Form

---
+++ 2_react/2_functional_components/hookform/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import RegistrationFormHooks from './components/RegistrationFormHooks';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h2>Registration Form</h2>
+      <p>Hook Form Assignment</p>
+
+      <RegistrationFormHooks/>
+
+      </div>
+  );
+  
+}
+
+export default App;
---
+++ 2_react/2_functional_components/hookform/src/components/RegistrationFormHooks.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+
+const RegistrationFormHooks = (props) =>{
+    const [firstName, setFirstName] = useState(props.firstName);
+    const [lastName, setLastName] = useState(props.lastName);
+    const [email, setEmail] = useState(props.email);
+    const [password, setPassword] = useState(props.password);
+    const [confPassword, setConfPassword] = useState(props.confPassword);
+
+    const onChangeHandler = (event) =>{
+        setFirstName( { firstName } );
+        setLastName( { lastName } );
+        setEmail( { email } );
+        setPassword( { password } );
+        setConfPassword( { confPassword } );
+
+    };
+
+    const moduleStyle = {
+        textAlign: "left",
+        marginLeft: "500px",
+        marginRight: "400px",
+        paddingLeft: "15px",
+        paddingBottom: "10px"
+    };
+
+    return(
+
+
+    <div>
+        <div className="divider">
+            <label htmlFor="firstName">First Name: </label>
+            <input
+                type="text"
+                value={ firstName }
+                onChange={ (event) => setFirstName( event.target.value ) }/>
+
+        </div>
+
+        <div className="divider">
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+            type="text"
+            value={ lastName }
+            onChange={ (event) => setLastName( event.target.value ) }/>
+        </div>
+
+        <div className="divider">
+        <label htmlFor="email">Email: </label>
+        <input
+            type="email"
+            value={ email }
+            onChange={ (event) => setEmail( event.target.value ) }/>
+        </div>
+
+        <div className="divider">
+        <label htmlFor="password">Password: </label>
+        <input
+            type="text"
+            value={ password }
+            onChange={ (event) => setPassword( event.target.value ) }/>
+        </div>
+
+
+        <div className="divider">
+        <label htmlFor="text">Confirm Password: </label>
+        <input
+            type="text"
+            value={ confPassword }
+            onChange={ (event) => setConfPassword( event.target.value ) }/>
+        </div>
+
+        <br></br>
+
+        <h3>Your Form Data:</h3>
+
+        <div style={ moduleStyle }>
+            <p>First Name: &emsp;<strong>{ firstName }</strong></p>
+            <p>Last Name: &emsp;<strong>{ lastName }</strong></p>
+            <p>Email: &emsp;<strong>{ email }</strong></p>
+            <p>Password: &emsp;<strong>{ password }</strong></p>
+            <p>Confirm Password: &emsp;<strong>{ confPassword }</strong></p>
+        </div>
+    </div>
+    );
+}
+export default RegistrationFormHooks;
