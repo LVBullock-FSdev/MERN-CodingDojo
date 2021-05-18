9ee13e34f2561c3086d26b96747f0654c930201a
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-05-18 14:34:26 -0400

    Commented out unused code

--- 2_react/2_functional_components/hookform/src/components/RegistrationFormHooks.js
+++ 2_react/2_functional_components/hookform/src/components/RegistrationFormHooks.js
@@ -7,14 +7,14 @@ const RegistrationFormHooks = (props) =>{
     const [password, setPassword] = useState(props.password);
     const [confPassword, setConfPassword] = useState(props.confPassword);
 
-    const onChangeHandler = (event) =>{
-        setFirstName( { firstName } );
-        setLastName( { lastName } );
-        setEmail( { email } );
-        setPassword( { password } );
-        setConfPassword( { confPassword } );
+    // const onChangeHandler = (event) =>{
+    //     setFirstName( { firstName } );
+    //     setLastName( { lastName } );
+    //     setEmail( { email } );
+    //     setPassword( { password } );
+    //     setConfPassword( { confPassword } );
 
-    };
+    // };
 
     const moduleStyle = {
         textAlign: "left",
