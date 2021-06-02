c4a1cdfbd8d8d3e38c2adf2f184302fe31e290dc
Laura V. Bullock <laura.v.bullock@gmail.com>
2021-06-01 21:53:17 -0400

    2-React --> 3-APIs --> Coin Flipping

---
+++ 2_react/3_apis/coinFlipping.js
@@ -0,0 +1,38 @@
+function tossCoin() {
+        return Math.random() > 0.5 ? "HEADS" : "tails";
+    }
+
+const fiveHeads = new Promise ( (resolve, reject) => {
+    let headsCount = 0;
+    let attempts = 0;
+    let maxAttempts = 100;
+
+    while( headsCount < 5 ) {
+        attempts++;
+        let result = tossCoin();
+        // console.log(`${result} was flipped`);
+
+        console.log( `Coin toss # ${attempts} -->\t ${result}` );
+
+            if( result === "HEADS") {
+                headsCount++;
+            } 
+            else {
+                headsCount = 0;
+            } 
+        }
+
+        if (attempts <= maxAttempts){
+            resolve( `It took ${attempts} coin tosses to get 5 "HEADS" consecutively.`);
+
+        } 
+        else{
+            reject( `Exceeded ${maxAttempts} coin tosses.` );
+        }
+    });
+
+    fiveHeads
+        .then( res => console.log(res) )
+        .catch( err => console.log(err) );
+
+    console.log( "When does this run now?" );  //what is this doing?????
